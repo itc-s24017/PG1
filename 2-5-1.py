ca5b8b37098e7ee4cc40b8aab1a7aaf16ebca58b
itc-s24017 <s24017@std.it-college.ac.jp>
2024-07-04 08:00:53 +0000

    7月４日に授業

---
+++ 2-5-1.py
@@ -0,0 +1,3 @@
+print(type(int('4')))
+print(float('9.8'))
+
---
+++ 2-6.py
@@ -0,0 +1,7 @@
+a = 'abc'
+b = 'xyz'
+print("{} {}".format(a, b))
+
+title = 'name'
+value = 'yuu dai'
+print( 'Your {} is {}.'.format(title, value))
